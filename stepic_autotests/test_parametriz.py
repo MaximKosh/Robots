91ff04cd8baf88e5ae35493b9fd9b578c04d30c7
MaximKosh <kmax.spb@mail.ru>
2022-04-04 19:01:22 +0300

    delete

--- stepic_autotests/test_parametriz.py
+++ stepic_autotests/test_parametriz.py
@@ -1,45 +1,39 @@
-import time
 import math
-import pytest
 import time
+
+import pytest
 from selenium import webdriver
 
 
-# @pytest.fixture(scope="function")
-# def browser():
-#     print("\nstart browser for test..")
-#     browser = webdriver.Chrome()
-#     yield browser
-#     print("\nquit browser..")
-#     time.sleep(3)
-#     browser.quit()
+@pytest.fixture(scope="function")
+def browser():
+    print("\nstart browser for test..")
+    browser = webdriver.Chrome()
+    yield browser
+    print("\nquit browser..")
+    time.sleep(5)
+    browser.quit()
 
 
-# @pytest.mark.parametrize('linkk', ["https://stepik.org/lesson/236895/step/1", "https://stepik.org/lesson/236896/step/1",
-#                                    "https://stepik.org/lesson/236897/step/1", "https://stepik.org/lesson/236898/step/1",
-#                                    "https://stepik.org/lesson/236899/step/1",
-#                                    "https://stepik.org/lesson/236903/step/1", "https://stepik.org/lesson/236904/step/1",
-#                                    "https://stepik.org/lesson/236905/step/1"])
-# def testitest(browser, linkk):
-#     link = {linkk}
-link = "https://stepik.org/lesson/236895/step/1"
-browser = webdriver.Chrome()
-browser.get(link)
-answer = math.log(int(time.time()))
-time.sleep(2)
- 
-a = browser.find_element_by_xpath("//div[@class='show-plugin']/div/textarea")
-a.send_keys(str(answer))
+@pytest.mark.parametrize('link', ["https://stepik.org/lesson/236895/step/1", "https://stepik.org/lesson/236896/step/1",
+                                  "https://stepik.org/lesson/236897/step/1", "https://stepik.org/lesson/236898/step/1",
+                                  "https://stepik.org/lesson/236899/step/1",
+                                  "https://stepik.org/lesson/236903/step/1", "https://stepik.org/lesson/236904/step/1",
+                                  "https://stepik.org/lesson/236905/step/1"])
+# вызываем фикстуру в тесте, передав ее как параметр
+class TestMainPage1():
 
-otpravka = browser.find_element_by_xpath("//div[@class='attempt__actions']/button").click
-# message = browser.find_element_by_id("verify_message")
-# assert "correct" in message.text
+    def test1(self, browser, link):
+        browser.get(link)
+        answer = math.log(int(time.time()))
+        # print(answer)
+        time.sleep(5)
+        link_1 = browser.find_element_by_xpath("//div/textarea")
+        link_1.send_keys(str(answer))
+        cont = browser.find_element_by_xpath("//div/section/div/div[1]/div[4]/button").click
+        message = browser.find_element_by_id("ember95")
+        assert "correct" in message.text
+        time.sleep(35)
 
-# https://stepik.org/lesson/236895/step/1
-# https://stepik.org/lesson/236896/step/1
-# https://stepik.org/lesson/236897/step/1
-# https://stepik.org/lesson/236898/step/1
-# https://stepik.org/lesson/236899/step/1
-# https://stepik.org/lesson/236903/step/1
-# https://stepik.org/lesson/236904/step/1
-# https://stepik.org/lesson/236905/step/1
+
+
